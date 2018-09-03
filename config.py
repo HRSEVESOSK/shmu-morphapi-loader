b8a615215f96c0386a82a28ce735c5fd374db102
klimeto <tomas.kliment@gmail.com>
2018-09-03 22:10:51 +0200

    config update

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 localhost = True
 server = False
-morphapikey = '2AMSouHpRtz5kxsTtmUX'
+morphapikey = 'KEYHERE'
 obsprop = {
     "ta_2m":1,
     "rh":2,
@@ -10,8 +10,8 @@ obsprop = {
     "wd_avg":6
 }
 if localhost:
-    dbhost = '5.189.139.50'
-    dbport = '5432'
-    dbname = 'sensorthings_sk'
-    dbuser = 'sensorthings'
-    dbpwd = 'sensorthings'
+    dbhost = 'HOSTHERE'
+    dbport = 'PORTHERE'
+    dbname = 'DBNAMEHERE'
+    dbuser = 'DBUSERHERE'
+    dbpwd = 'DBPASSHERE'
