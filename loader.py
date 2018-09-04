e63c0320f9b8bb453c36d2167a33a9a13a1f7ad7
klimeto <tomas.kliment@gmail.com>
2018-09-04 08:28:05 +0200

    order by RESULT_TIME cast changed from date to timestamptz

--- loader.py
+++ loader.py
@@ -4,7 +4,6 @@ morph_api_url = "https://api.morph.io/soit-sk/scraper-shmu-observations/data.jso
 morph_api_key = config.morphapikey
 YESTERDAY = (datetime.today() - timedelta(1)).isoformat()
 OBSPROP = json.loads(json.dumps(config.obsprop))
-
 QUERY = "SELECT * FROM data WHERE date >= '%s' ORDER BY date ASC" % YESTERDAY
 r = requests.get(morph_api_url, params={
   'key': morph_api_key,
@@ -28,11 +27,11 @@ if len(results) > 0:
                 connection.close()
                 if DSID:
                     # GET LAST INSERT DATE
-                    sql = """select to_char("RESULT_TIME"::timestamptz at time zone 'UTC', 'YYYY-MM-DD"T"HH24:MI:SS"Z"') as "LAST_DATE" from "OBSERVATIONS2" where "DATASTREAM_ID" = %s order by "RESULT_TIME"::date DESC limit 1""" % DSID[0][0]
+                    sql = """select to_char("RESULT_TIME"::timestamptz at time zone 'UTC', 'YYYY-MM-DD"T"HH24:MI:SS"Z"') as "LAST_DATE" from "OBSERVATIONS2" where "DATASTREAM_ID" = %s order by "RESULT_TIME"::timestamptz DESC limit 1""" % DSID[0][0]
                     connection.connect()
                     LASTDATE = connection.query(sql, False)
                     connection.close()
-                    if datetime.strptime(RESTIME, '%Y-%m-%dT%H:%M:%SZ') > datetime.strptime(LASTDATE[0][0], '%Y-%m-%dT%H:%M:%SZ'):  # 2018-09-01T21:00:00Z
+                    if datetime.strptime(RESTIME, '%Y-%m-%dT%H:%M:%SZ') > datetime.strptime(LASTDATE[0][0], '%Y-%m-%dT%H:%M:%SZ'):  # 2018-09-01T21:00:00Z)
                         if v is not None:
                             print("%s - INFO: INSERTING NEW OBSERVATION VALUE '%s' FOR PROPERTY_ID '%s' WITH TIMESTAMP '%s' FOR DS ID '%s' DS DESCRIPTION '%s'" % (datetime.now().isoformat(),v,OBSPROP[k], RESTIME, DSID[0][0], DSID[0][1]))
                             sql = """insert into "OBSERVATIONS2"("RESULT_TIME","RESULT_NUMBER","DATASTREAM_ID","FEATURE_ID") VALUES ('%s',%s,%s,%s) RETURNING "ID" """ % (RESTIME,v,DSID[0][0],DSID[0][2])
@@ -44,7 +43,7 @@ if len(results) > 0:
                         else:
                             print("%s - INFO: PARAMETER '%s' HAS VALUE: '%s'" % (datetime.now().isoformat(),k,v))
                     else:
-                        print("%s - INFO: RECENT DATA ALREADY STORED IN DB FOR DS ID %s" % (datetime.now().isoformat(), DSID[0][0]))
+                        print("%s - INFO: OBSERVATION FOR TIME %s ALREADY STORED IN DB FOR DS ID %s" % (datetime.now().isoformat(),datetime.strptime(RESTIME, '%Y-%m-%dT%H:%M:%SZ'), DSID[0][0]))
                 else:
                     print("%s - WARNING: THING NAME '%s' HAS NOT OBSERVED PROPERTY '%s' " % (datetime.now().isoformat(), THGNAME, OBSPROP[k]))
     print("DATA COLLECTTION PROCESS END ON %s" % datetime.now().isoformat())
